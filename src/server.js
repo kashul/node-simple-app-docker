56263ea3ab651919c1840b90ead9c359ef4f5e16
Kashul <kashul@climatesi.com>
2024-04-27 21:17:38 +0530

    update dockerFile

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
+
 const port = 3000;
 
 const server = createServer((req, res) => {
-    console.log(`work`);
+ 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running listning to port ${port}`);
 });
